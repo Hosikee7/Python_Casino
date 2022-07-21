971312078008c776c1f18536f3489ee12ffee26d
Hosikee7 <hse101010@naver.com>
2022-07-21 20:26:08 +0900

    Update Code!
    New System : Coin System

--- py_casino.py
+++ py_casino.py
@@ -1,8 +1,11 @@
+from audioop import add
 import random
 
-a = input("숫자를 입력하시오. (1부터 30까지의 수) : ")
+money = 1000
 
-lst1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30']
+a = input("숫자를 입력하시오. (1부터 15까지의 수) : ")
+
+lst1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
 lst2 = ['Wow!', 'Awsome!', 'Great']
 lst3 = ['Oops', 'Try one more!', 'Oh, no']
 
@@ -13,6 +16,8 @@ print("AI : " + ai_answer + ", You : " + a)
 if a == ai_answer:
     a = random.choice(lst2)
     print(a)
+    print("Your Pocket :", money + 10)
 else:
     b = random.choice(lst3)
     print(b)
+    print("Your Pocket :", money - 30)
